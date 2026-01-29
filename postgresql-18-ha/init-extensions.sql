c085001c6b347f11a1083522af966ffc186908d9
heyjaypray <hey@jaypray.com>
2026-01-28 22:25:17 -0700

    update

--- postgresql-18-ha/init-extensions.sql
+++ postgresql-18-ha/init-extensions.sql
@@ -7,7 +7,10 @@ CREATE EXTENSION IF NOT EXISTS "unaccent";
 -- Main Feature Extensions
 CREATE EXTENSION IF NOT EXISTS "postgis";
 CREATE EXTENSION IF NOT EXISTS "vector"; -- pgvector
-CREATE EXTENSION IF NOT EXISTS "pg_partman";
+
+-- Install pg_partman in its own schema to avoid conflicts with ORMs like Strapi
+CREATE SCHEMA IF NOT EXISTS partman;
+CREATE EXTENSION IF NOT EXISTS "pg_partman" SCHEMA partman;
 
 -- NOTE: pg_cron is created in entrypoint.sh background task
 -- because it requires shared_preload_libraries which isn't loaded
